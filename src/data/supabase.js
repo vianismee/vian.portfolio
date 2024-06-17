c998de2bec65660615fe25b9b5b3452a993cf989
vianismee <104370516+vianismee@users.noreply.github.com>
2024-06-17 12:05:34 +0700

    Supabase eror

--- src/data/supabase.js
+++ src/data/supabase.js
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ANON_KEY);
+export const supabase = createClient('https://sfoojamrmgvnpqymiild.supabase.co', import.meta.env.SUPABASE_ANON_KEY);
